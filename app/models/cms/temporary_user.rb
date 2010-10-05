1c832b56b8210738c8be9e21f598fb054c215f71
peakpg <peakpg@gmail.com>
2010-10-05 12:18:13 -0400

    Fixed bug where empty groups would cause cms_access? to return an array rather than false. Fixed bug where TemporaryUser#able_to_view? wasn't looking at groups

--- app/models/cms/temporary_user.rb
+++ app/models/cms/temporary_user.rb
@@ -2,6 +2,13 @@ module Cms
 
   # This represents a 'temporary' user who is never stored in the database, but lives only as a limited duration (generally tied to a session)
   # This can be used to represent an externally verified user who should be granted access to areas of the site.
+  #
+  #
+  # Typical Usage:
+  #
+  # user = Cms::TemporaryUser.new(:login=>"bob@externalurl.com")
+  # user << Group.find_by_code("Special Group")
+  # user.able_to_view?("/some/path")
   class TemporaryUser < User
 
     # Shouldn't save these users to the db.
@@ -22,6 +29,22 @@ module Cms
       groups.each do |g|
         return true if g.cms_access?
       end
+      false
+    end
+
+#    def cms_access?
+#      groups.each do |g|
+#        return true if g.cms_access?
+#      end
+#      false
+#    end
+
+    # Returns all sections that this user has rights to view.
+    #
+    # Overrides core behavior of User to avoid including user as part of the query (since temp users doen't exist in the database).
+    #
+    def viewable_sections
+      Section.find(:all, :joins=>:groups, :conditions=>["groups.id IN (?)", groups])
     end
   end
 end
--- test/unit/cms/temporary_user_test.rb
+++ test/unit/cms/temporary_user_test.rb
@@ -29,6 +29,45 @@ class TemporaryUserTest < ActiveSupport::TestCase
   test "Should belong to no groups by default" do
     assert_equal 0, @user.groups.size
   end
+
+  test "Temp users should be able to view sections their groups have permissions to" do
+    group = Group.create!(:name=>"My Group")
+    section = Section.create!(:name=>"My Section", :path=>"/mysection")
+    section.groups << group
+    section.save!
+
+    user = Cms::TemporaryUser.new
+    user.groups << group
+
+    assert user.able_to_view?(section)
+
+
+  end
+
+  test "Viewable sections" do
+    group = Group.create!(:name=>"Group 1")
+    first_section = Section.create!(:name=>"Section1", :path=>"/section1")
+    first_section.groups << group
+    first_section.save!
+
+    group2 = Group.create!(:name=>"Group 2")
+    second_section = Section.create!(:name=>"Section 2", :path=>"/section2")
+    second_section.groups << group2
+    second_section.save!
+
+    user = Cms::TemporaryUser.new
+    user.groups << group << group2
+
+    assert_equal [first_section, second_section], user.viewable_sections
+    assert user.able_to_view?(first_section)
+    assert user.able_to_view?(second_section)
+
+  end
+
+  test "A user with no groups should not have CMS access" do
+    user = Cms::TemporaryUser.new
+    assert_equal false, user.cms_access? 
+  end
 end
 
 
